6e33e84212e156307e175d514e75181ace5e1a08
fclaiba <f.claisse.r@gmail.com>
2024-09-12 18:26:56 -0300

    3

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <CEOPresentation />
       <TradingSkillsImprovement />
       <TestimonialsAndScreenshots />
-      <VideoCta />
       <FAQAccordion />
+      <VideoCta />
       <Footer />
     </>
   );
